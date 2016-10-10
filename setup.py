dd8e8ab765bb06d14bd5c5eeb301e31e24c9db89
j <j.gao@aaa.net>
2016-10-10 13:58:27 +0100

    added install file setup.py for Docker container building

---
+++ setup.py
@@ -0,0 +1,13 @@
+﻿#! /usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='papy',
+      version='0.1',
+      description='Power Analysis tool in Python',
+      author='Gonçalo Correia, Jianliang Gao',
+      author_email='j.gao@imperial.ac.uk',
+      url='',
+      scripts=["papy/pa.py"],
+      install_requires=["numpy","scipy","joblib","multiprocessing", "statsmodels", "matplotlib"],
+      packages=find_packages(),
+      )
