05f45f063b00accd263933d18eed4e0681ba9cb2
j <j.gao@aaa.net>
2017-03-01 14:56:25 +0000

    updated setup.py to get 1 more script

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='papy',
       author='Gonc¸alo Correia, Jianliang Gao',
       author_email='j.gao@imperial.ac.uk',
       url='',
-      py_modules=['papy/pa','papy/plotSurface'],
+      scripts=["papy/pa.py","papy/plotSurface.py"],
       install_requires=["numpy","scipy","joblib","multiprocessing", "statsmodels", "matplotlib", "plotly"],
       packages=find_packages(),
       )
