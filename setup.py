315b50efa49ba7f44faed4483b9e7183aabcf591
j <j.gao@aaa.net>
2017-03-05 09:20:15 +0000

    updated software version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='papy',
-      version='3.0',
+      version='5.0',
       description='Power Analysis tool in Python',
       author='Gonc¸alo Correia, Jianliang Gao',
       author_email='j.gao@imperial.ac.uk',
