1f0f43c33e287aaf8e2468eb6b85a7472cdf378b
j <j.gao@aaa.net>
2017-03-16 08:00:32 +0000

    updated setup file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='papy',
-      version='0.1',
+      version='5.0',
       description='Power Analysis tool in Python',
-      author='Gonçalo Correia, Jianliang Gao',
+      author='Gonc竌lo Correia, Jianliang Gao',
       author_email='j.gao@imperial.ac.uk',
       url='',
-      scripts=["papy/pa.py"],
-      install_requires=["numpy","scipy","joblib","multiprocessing", "statsmodels", "matplotlib"],
+      scripts=["papy/pa.py","papy/plotSurface.py"],
+      install_requires=["numpy","scipy","joblib","multiprocessing", "statsmodels", "matplotlib", "plotly"],
       packages=find_packages(),
       )
